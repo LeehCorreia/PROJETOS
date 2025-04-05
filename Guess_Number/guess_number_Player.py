48806ed6ea972b58f654a72e220b427c0043419f
LeehCorreia <aledark1810@gmail.com>
2025-04-05 11:31:27 -0300

    GuessPart2

--- Guess_Number/guess_number_Player.py
+++ Guess_Number/guess_number_Player.py
@@ -1,5 +1,20 @@
 import random
 
 def guess(x):
-    random_guess = random.randint(1,x)
-    number = input("Informe one number between 1 and {x}: ")
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is {guess} too high (H), too low (L), or correct (C)?").lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print(f"The computer guessed your number, {guess}, correctly!")
+
+guess(20)
