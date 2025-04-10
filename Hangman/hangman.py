44f8e2fb0bfd4d73d87e5e87649da4b1d1a2fd60
LeehCorreia <aledark1810@gmail.com>
2025-04-10 20:51:57 -0300

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -4,7 +4,7 @@ import string
 
 def get_valid_word(words):
     word = random.choice(words)
-    print(word)
+    # print(word)
     while '-' in word or ' ' in word:
         word = random.choice(words)
     
@@ -18,22 +18,24 @@ def hangman():
 
     while len(word_letters) > 0:
         print('You have used these letters: ',' '.join(used_letters))
-
-        user_letter = input('Guess a letter: ').upper()
-
         word_list = [letter if letter in used_letters else '-' for letter in word]
+
         print('Current word: ', ' '.join(word_list))
+        print()
+        user_letter = input('Guess a letter: ').upper()
 
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
             if user_letter in word_letters:
-                word_letters.remove(used_letters)
+                word_letters.remove(user_letter)
 
         elif user_letter in used_letters:
             print('You hava already used that character. Please guess again.')
         
         else:
             print('Invalid character. Please try a valid character.')
+        if len(word_letters) == 0:
+            print(word)
 
-user_input = input("Type something: ")
-print(user_input)
+    
+hangman()
