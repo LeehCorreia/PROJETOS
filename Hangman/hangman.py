841c0b4672d7c8c37070088360912011fe86ddd9
LeehCorreia <aledark1810@gmail.com>
2025-04-10 18:02:52 -0300

    Hangman

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -14,5 +14,26 @@ def hangman():
     word = get_valid_word(words)
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
+    used_letters = set()
 
+    while len(word_letters) > 0:
+        print('You have used these letters: ',' '.join(used_letters))
 
+        user_letter = input('Guess a letter: ').upper()
+
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(used_letters)
+
+        elif user_letter in used_letters:
+            print('You hava already used that character. Please guess again.')
+        
+        else:
+            print('Invalid character. Please try a valid character.')
+
+user_input = input("Type something: ")
+print(user_input)
