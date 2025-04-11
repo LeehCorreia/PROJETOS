8e285bdcb918a6cd4239faa56b138e3753775467
LeehCorreia <aledark1810@gmail.com>
2025-04-10 21:04:11 -0300

    Joken

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -3,12 +3,12 @@ import random
 def game():
     player = input("'r' for rock, 'p' for paper, 's' for scissors: ").lower()
     computer = random.choice(['r', 'p', 's'])
-        if computer == 'r':
-            print("Computer used ROCK!")
-        elif computer == 'p':
-            print("Computer used PAPER!")
-        else:
-            print('Computer used SCISSORS!')
+    if computer == 'r':
+        print("Computer used ROCK!")
+    elif computer == 'p':
+        print("Computer used PAPER!")
+    else:
+        print('Computer used SCISSORS!')
     
     if player == computer:
         return 'It\'s a tie!'
