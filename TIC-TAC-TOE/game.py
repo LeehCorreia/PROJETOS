ee24e7d9af51d93461924fb7b0baf47b121e7fd9
LeehCorreia <aledark1810@gmail.com>
2025-04-13 15:59:02 -0300

    TICTACTOE

--- TIC-TAC-TOE/game.py
+++ TIC-TAC-TOE/game.py
@@ -1,3 +1,4 @@
+import math
 import time
 from player import HumanPlayer, RandomComputerPlayer
 
@@ -94,6 +95,6 @@ def play(game, x_player, o_player, print_game=True):
 
 if __name__ == '__main__':
     x_player = HumanPlayer('X')
-    o_player = RandomComputerPLayer('O')
+    o_player = RandomComputerPlayer('O')
     t = TicTacToe()
     play(t, x_player, o_player, print_game=True)
--- TIC-TAC-TOE/player.py
+++ TIC-TAC-TOE/player.py
@@ -25,13 +25,13 @@ class HumanPlayer(Player):
         val = None
         while not valid_square:
             square = input(self.letter + '\'s turn. Input move (0-8):')
-        try:
-            val = int(square)
-            if val not in game.avaliable_moves():
-                raise ValueError
-            valid_square = True
-        except ValueError:
-            print('Invalid square. Try again.')
-    
+            try:
+                val = int(square)
+                if val not in game.avaliable_moves():
+                    raise ValueError
+                valid_square = True
+            except ValueError:
+                print('Invalid square. Try again.')
+        
         return val
 
