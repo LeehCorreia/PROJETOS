1788484c0e76e1b8709d953b44e904e5e8543f59
LeehCorreia <aledark1810@gmail.com>
2025-04-13 15:45:29 -0300

    TicTacToe

--- TIC-TAC-TOE/player.py
+++ TIC-TAC-TOE/player.py
@@ -5,5 +5,33 @@ class Player:
     def __init__(self, letter):
         self.letter = letter
 
+    def get_move(self, game):
+        pass
 
-        
+class RandomComputerPLayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        square = random.choice(game.avaliable_moves())
+        return square
+
+class HumanPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+    
+    def get_move(self, game):
+        valid_square = False
+        val = None
+        while not valid_square:
+            square = input(self.letter + '\'s turn. Input move (0-8):')
+        try:
+            val = int(square)
+            if val not in game.avaliable_moves():
+                raise ValueError
+            valid_square = True
+        except ValueError:
+            print('Invalid square. Try again.')
+    
+    return val
+
